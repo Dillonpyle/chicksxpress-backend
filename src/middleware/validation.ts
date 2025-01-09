d45104d1718b2fd4cb4f4e587b3f0e4b9e3f7a48
Dillonpyle <dillonpyle1991@gmail.com>
2025-01-09 17:53:44 -0600

    added validation to restaurant route

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -5,7 +5,7 @@ const handleValidationErrors = async (
     req: Request, 
     res: Response, 
     next: NextFunction
-): Promise<void> => {
+    ): Promise<void> => {
     const errors = validationResult(req);
 
     if(!errors.isEmpty()){
@@ -20,5 +20,18 @@ export const validateMyUserRequest = [
     body("addressLine1").isString().notEmpty().withMessage("Address Line 1 must be a string"),
     body("city").isString().notEmpty().withMessage("City must be a string"),
     body("country").isString().notEmpty().withMessage("Country must be a string"),
+    handleValidationErrors,
+]
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").isString().notEmpty().withMessage("Restaurant name is required"),
+    body("city").isString().notEmpty().withMessage("City is required"),
+    body("country").isString().notEmpty().withMessage("Country is required"),
+    body("deliveryPrice").isFloat({ min: 0}).notEmpty().withMessage("Delivery price must be a postive value"),
+    body("estimatedDeliveryTime").isInt({ min: 0}).notEmpty().withMessage("Estimated delivery time must be a positive value"),
+    body("cuisines").isArray().not().isEmpty().withMessage("Cuisines array cannot be empty"),
+    body("menuItems").isArray().withMessage("Menu items must be an array"),
+    body("menuItems.*.name").isString().notEmpty().withMessage("Menu item name is required"),
+    body("menuItems.*.price").isFloat({min: 0}).notEmpty().withMessage("Menu item price is required"),
     handleValidationErrors,
 ]
